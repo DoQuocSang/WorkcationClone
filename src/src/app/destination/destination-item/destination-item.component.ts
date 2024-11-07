bf3581a3763fb21a3d2c299f78e80161bde80c23
DoQuocSang <quocsang316@gmail.com>
2024-11-07 14:36:45 +0700

    Điều chỉnh cấu trúc file

---
+++ src/src/app/destination/destination-item/destination-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-destination-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './destination-item.component.html',
+})
+export class DestinationItemComponent {
+
+}
