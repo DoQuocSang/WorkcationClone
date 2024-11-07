6a9d3ac137ef31610d5e85aafd2f522a7f5b1f15
DoQuocSang <quocsang316@gmail.com>
2024-11-07 17:27:10 +0700

    Xong header 90%, còn bug absolute

--- src/src/app/header/header.component.ts
+++ src/src/app/header/header.component.ts
@@ -1,11 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
+  isShow = false;
+  svgColor = "#e5e7eb";
 
+  showMenu(){
+    this.isShow = true;
+  }
+
+  hideMenu(){
+    this.isShow = false;
+  }
 }
