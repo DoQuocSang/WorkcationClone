d04fe32d2ccf6a05680b82301f9eceb6c007c1ff
DoQuocSang <quocsang316@gmail.com>
2024-11-08 09:40:57 +0700

    Hoàn thành hero và header

--- src/src/app/app.component.ts
+++ src/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import {
+  DestinationGridComponent,
+} from './destination/destination-grid/destination-grid.component';
 import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, DestinationGridComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
